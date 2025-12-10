e3a3e5049238504a83b6882b3e53befc1dd8d668
altisss0806 <78189462+altisss0806@users.noreply.github.com>
2025-12-10 23:20:22 +0700

    Add files via upload

--- oauth.js
+++ oauth.js
@@ -7,7 +7,7 @@ const CONFIG = {
     CLIENT_ID: '9129744248003.10087834431718',
     CLIENT_SECRET: '9ecc93f5bb1f8d7a7f247364970c03df',
     // App-Level Token for Socket Mode (xapp-...)
-    APP_TOKEN: 'xapp-1-A0A2KQJCPM4-10078796510919-42d11dc27be57ce384d06e10e7fb9199a7e2ea19252c23c27eb66eb26567e515',
+    APP_TOKEN: 'xapp-1-A0A2KQJCPM4-10092343186438-9bd6d07d1042b11e7347485571f8d221feee27e9404828cc9df5ead8bc63b6d3',
     // User scopes for OAuth - these are the permissions required
     USER_SCOPES: 'channels:read,channels:history,chat:write,groups:read,groups:history,im:read,im:history,users:read,files:read',
     // This should match your GitHub Pages URL
